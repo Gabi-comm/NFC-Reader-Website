05d62fc44c99965f5b378d21d18c07c0aa307d20
kurakaka <abi8905.del@gmail.com>
2025-12-01 19:19:02 +0800

    Welcom Sequence Done
    okay so napalit ko yung App/ yung dati, as Success/ na, paayus nalang HUHU, OK change mo file path ng ICON sa Success.css kasi absolute file path ginamit para gumana

---
+++ src/Success.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import './Success.css';
+import { Container, Row, Col } from "reactstrap";
+
+function Success() {
+    return (
+        <section className='background'>
+            <Container className='flex-center'>
+                <section className='flex-border'>
+                    <Row>
+                        <Col className='flex-container '>
+                            <div className='icon-logo'></div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <h1 className='center heading-sub'>Welcome</h1>
+                            <h2 className='center heading-main'>Association of Computer Science</h2>
+                        </Col>
+                    </Row>
+                </section>
+
+            </Container>
+        </section>
+
+    );
+}
+
+export default Success;
---
+++ src/Success.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import Success from './Success';
+
+test('renders learn react link', () => {
+  render(<Success />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
