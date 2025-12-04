4d11458f6fab88202c38f90347491105b3c092d1
kurakaka <abi8905.del@gmail.com>
2025-12-04 20:53:35 +0800

    MAIN PAGE

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -3,16 +3,68 @@ import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './Mainpage.css';
 import { Container, Row, Col } from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 function Main() {
     return (
-        <section className='background'>
-            <Container>
-                <Row>
-                    <div className='icon-logo'></div>
+        <section >
+            <Container fluid="xxl" >
+                <Row >
+                    <Col xs='12' lg='3' className='sidebar-nav'>
+                        <section className='logo-section'>
+                            <div className='icon-logo'></div>
+                            <p>Associaton of Computer Studies Students</p>
+                        </section>
+                        <section>
+                            <ListGroup className='list-group '>
+                                <ListGroupItem
+                                    action
+                                    className='list-item '
+                                    href="#"
+                                    tag="a"
+                                >
+                                    Register
+                                </ListGroupItem>
+                                <ListGroupItem
+                                    action
+                                    className='list-item'
+                                    href="#"
+                                    tag="a"
+                                >
+                                    Officers
+                                </ListGroupItem>
+                                <ListGroupItem
+                                    action
+                                    className='list-item'
+                                    href="#"
+                                    tag="a"
+                                >
+                                    Members
+                                </ListGroupItem>
+                                <ListGroupItem
+                                    action
+                                    className='list-item'
+                                    href="#"
+                                    tag="a"
+                                >
+                                    Events
+                                </ListGroupItem>
+
+                            </ListGroup>
+                        </section>
+                        <section className='logout-section'>
+                            <div className='moab-logo'></div>
+                            <p><a href='#'>Log-Out</a></p>
+
+                        </section>
+                    </Col>
+                    <Col xs='12' lg='9' className='bg-dark main-section'>
+
+                    </Col>
                 </Row>
+
             </Container>
-        </section>
+        </section >
 
     );
 }
--- src/Success.js
+++ src/Success.js
@@ -13,13 +13,18 @@ function Success() {
                         <Col className='flex-container '>
                             <div className='icon-logo'></div>
                         </Col>
-                    </Row>
-                    <Row>
                         <Col>
                             <h1 className='center heading-sub'>Welcome</h1>
                             <h2 className='center heading-main'>Association of Computer Science</h2>
                         </Col>
+                        <Col>
+                            <div className='moab-logo'></div>
+                            <a>LogOut</a>
+                        </Col>
                     </Row>
+
+
+
                 </section>
 
             </Container>
