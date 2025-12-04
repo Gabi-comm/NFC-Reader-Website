c1679cc488ddaf91afd75d98377dfb6823f04d0e
kurakaka <abi8905.del@gmail.com>
2025-12-04 23:36:13 +0800

    main page design

--- src/Mainpage.js
+++ src/Mainpage.js
@@ -10,7 +10,7 @@ function Main() {
         <section >
             <Container fluid="xxl" >
                 <Row >
-                    <Col xs='12' lg='3' className='sidebar-nav'>
+                    <Col xs='12' lg='2' className='sidebar-nav'>
                         <section className='logo-section'>
                             <div className='icon-logo'></div>
                             <p>Associaton of Computer Studies Students</p>
@@ -54,11 +54,11 @@ function Main() {
                         </section>
                         <section className='logout-section'>
                             <div className='moab-logo'></div>
-                            <p><a href='#'>Log-Out</a></p>
+                            <a href='#'>Log-Out</a>
 
                         </section>
                     </Col>
-                    <Col xs='12' lg='9' className='bg-dark main-section'>
+                    <Col xs='12' lg='10' className='bg-dark main-section'>
 
                     </Col>
                 </Row>
